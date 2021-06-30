6902c65df5fe0d18fd2c5e67c42b51f03960f79b
AdamV97 <adam-vukelic@hotmail.com>
2021-07-01 00:36:11 +0200

    Added notifications
    add: - Added functions for handling push notifications to android devices

--- modules/mqttMotionReader.js
+++ modules/mqttMotionReader.js
@@ -1,6 +1,7 @@
 const mqtt = require('mqtt');
 const mysql = require('mysql');
 const fs = require('fs');
+const {admin} = require('../firebase-config.js');
 
 //Read json with server data
 let rawdata = fs.readFileSync('data.json');
@@ -57,9 +58,66 @@ function checkTime(){
   return true;
 };
 
+function getInstallationTokens(){
+  return new Promise(resolve => {
+    let tokens = [];
+    let sql = "SELECT token FROM installation_token"
+
+    con.query(sql, function (err, result, fields) {
+      if (err) throw err;
+      for(let i = 0; i < result.length; i++){
+        tokens.push(result[i].token);
+      }
+      resolve(tokens);
+    });
+  });
+}
+
+async function sendNotification(){
+  const tokens = await getInstallationTokens();
+
+  const message = {
+    notification: {
+      title: 'Motion Sensor!',
+      body: 'Somebody triggerd the motion sensor!'
+    },
+    android:{
+      priority:"high"
+    },
+    tokens: tokens
+  };
+  
+  admin.messaging().sendMulticast(message, false)
+  .then((response) => {
+    console.log(response.successCount + ' users have been alerted');
+  });
+}
+
+function getActiveAlarms(){
+  return new Promise(resolve => {
+    let sql = "SELECT active FROM alarm WHERE id = 3"
+
+    con.query(sql, function (err, result, fields) {
+      if (err) throw err;
+
+      resolve(result[0].active);
+
+    });
+  });
+}
+
+async function triggerAlarms(){
+  const isActive = await getActiveAlarms();
+  if(isActive){
+    sendNotification();
+  }
+}
+
 client.on('message', async (topic, message) => {
   let motionSensorData = JSON.parse(message.toString());
   motionSensorData.topic = topic;
+
+  triggerAlarms();
 
   if(!motionSensorData.occupancy){
     client.publish('zigbee2mqtt/0x842e14fffe3597db/set', '{"state": "OFF"}');
